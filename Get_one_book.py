84002a5fb0134dec89f8fd25858e96504ad7939e
newkeros <60075901+newkeros@users.noreply.github.com>
2020-10-23 19:33:29 +0200

    Add files via upload

--- Get_one_book.py
+++ Get_one_book.py
@@ -1,20 +1,43 @@
-# request va récupérer les données html
-r = requests.get("http://books.toscrape.com/catalogue/how-music-works_979/index.html")
-# BS va récupérer le texte avec lxml pour parser les infos
-soup = BeautifulSoup(r.text, 'lxml')
-# On cherche ce qu'on veut avec soup.find dans le code html
-article = soup.find('article')
+from main import *
 
-# print(article.prettify())
+def get_product_page_url():
+    page_url = str('http://books.toscrape.com/catalogue/how-music-works_979/index.html')
+    print(page_url)
 
-def get_title()
+
+def get_title(article):
     title = article.find('div', class_='col-sm-6 product_main').h1.text
     print(title)
-def get_product_informations()
-    table_infos = len(article.find('table', class_='table table-striped'))
-    print(table_infos[0,1,4,5,6,7])
-def get_product_description()
-    description = article.find('div', class_='sub-header').h2.text
-    print(description)
-    product_description_text = article.find('div', id_='product_description')
-    print(product_description_text)
+
+
+def get_product_upc(article):
+    tableau = article.select("tr")
+    print(tableau[0].td.text)
+
+
+def get_product_description(article):
+    product_description_text = article.select("p")
+    print(product_description_text[3].text)
+
+
+def get_price_including_tax(article):
+    price_with_tax = article.select("tr")
+    print(price_with_tax[3].td.text)
+
+if __name__ == "__main__":
+    # request va récupérer les données html
+    r = requests.get("http://books.toscrape.com/catalogue/how-music-works_979/index.html")
+    # BS va récupérer le texte avec lxml pour parser les infos
+    soup = BeautifulSoup(r.text, 'lxml')
+    # On cherche ce qu'on veut avec soup.find dans le code html
+    article = soup.find('article')
+    get_product_page_url()
+    get_product_upc(article)
+    get_title(article)
+    get_price_including_tax(article)
+    #get_price_excluding_tax(article)
+    #get_number_available(article)
+    get_product_description(article)
+    #get_category(article)
+    #get_review_rating(article)
+    #get_image_url(article)
