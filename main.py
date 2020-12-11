1f1487ccc72be863e27957f4546f6fbdc404c23f
newkeros <60075901+newkeros@users.noreply.github.com>
2020-12-11 08:15:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from csv import DictWriter
 from Get_one_book import get_all_product_infos
 from Get_category_books import *
 from CSV_creator import create_csv
+from Request import request
 
 
 
 if __name__ == "__main__":
-  #create_csv(get_all_product_infos("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"), "greg")
-  #create_csv(get_all_product_infos("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"), "greg")
 
-    sequential_art_category = get_category_books("https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html")
-    for urls in sequential_art_category:
-        print(urls)
+    category_url_list = get_category_books("https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html")
+    for urls in category_url_list:
+        request(urls)
+        category_dict = get_all_product_infos(urls)
+        create_csv(category_dict, "Sequential art")
+
+
 
 
 
